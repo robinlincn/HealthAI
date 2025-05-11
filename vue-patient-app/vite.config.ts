0c5c8bef1fc7509da4499cd635c78e229ed76f8b
robin lin <robinlincn@gmail.com>
2025-05-11 02:41:01 +0000

    点击打开VUE版后报：Internal Server Error

--- vue-patient-app/vite.config.ts
+++ vue-patient-app/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     port: 9003, // Specify port for Vue app
+    host: '0.0.0.0', // Listen on all network interfaces
   },
 })
